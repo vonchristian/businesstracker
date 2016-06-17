7809cd3d0b69d03b3a8e3e82bc4c8580baae8016
Von Christian Halip <vc.halip@gmail.com>
2016-06-17 14:56:51 +0800

    Lots of changes

---
+++ app/controllers/landing_page_controller.rb
@@ -0,0 +1,5 @@
+class LandingPageController < ApplicationController
+  layout 'landing_page'
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'landing_page/index'
+get 'home', to: 'landing_page#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
